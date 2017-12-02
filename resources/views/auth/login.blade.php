80f2a76195f36408271d9d911feba35131060bb5
nickverstocken <verstockennick@gmail.com>
2017-12-02 18:57:26 +0100

    login, registreer and start layout done!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,19 +2,28 @@
 
 @section('content')
     <div class="flex-center full-height main" id="login">
-        <div class="intro">
-            <h1>Login</h1>
-            <div class="form">
-                <input  />
-            </div>
-            <div class="buttons">
-                <button class="button orange">Begin nu</button><button class="button white">Log in</button>
-            </div>
-            <img class="animal-bg hagedis" src="{{ URL::asset('images/hagedis.svg') }}" alt="Hagedis">
-        </div>
-        <img class="animal-bg olifant" src="{{ URL::asset('images/olifant.svg') }}" alt="Olifant">
-        <img class="animal-bg krokodil" src="{{ URL::asset('images/krokodil.svg') }}" alt="Olifant">
-        <img class="animal-bg giraf" src="{{ URL::asset('images/giraf.svg') }}" alt="Giraf">
-    </div>
+        <div class="mainwrap">
+            <div class="intro">
+                <h1>Login</h1>
+                <div class="form">
+                    <form >
+                        <input type="email" placeholder="Email" name="email" required >
+                        <input type="password" placeholder="Wachtwoords" name="password" required autocomplete="off">
+                    </form>
 
+                </div>
+                <div class="buttons">
+                    <button type="submit" class="button orange">Log in</button>
+                    <a href="/registreer" class="button white">Registreer</a>
+                </div>
+                <img class="animal-bg hagedis" src="{{ URL::asset('images/hagedis.svg') }}" alt="Hagedis">
+
+            </div>
+            <div class="bg-animals">
+                <img class="animal-bg olifant" src="{{ URL::asset('images/olifant.svg') }}" alt="Olifant">
+                <img class="animal-bg krokodil" src="{{ URL::asset('images/krokodil.svg') }}" alt="Olifant">
+                <img class="animal-bg giraf" src="{{ URL::asset('images/giraf.svg') }}" alt="Giraf">
+            </div>
+        </div>
+    </div>
 @stop
