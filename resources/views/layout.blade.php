7c9b83fbc32f785d787978bac563f71fdf68916a
nickverstocken <verstockennick@gmail.com>
2017-12-02 16:44:04 +0100

    start page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Open+Sans+Condensed:300,700" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+    </body>
+</html>
