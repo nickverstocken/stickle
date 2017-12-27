3d51415b7296cd8a1e3289eb5f7acc93f67223f5
lekevinbm <kevin.bavuidi@hotmail.com>
2017-12-27 12:28:59 +0100

    Begin boek toevoegen

--- resources/views/layouts/modals/book.blade.php
+++ resources/views/layouts/modals/book.blade.php
@@ -10,23 +10,28 @@
         </div>
         <div class="modalContent">
             <div class="contentContainer">
-                <form id="addBookForm" name="addBookForm">
-                    <div class="card">
-                        <h4>Auteur</h4>
-                        <input type="text" placeholder="Auteur*" autocorrect="off" autocapitalize="off" spellcheck="off" class="input">
-                    </div>
+                <form id="addBookForm" name="addBookForm" method="POST" action="/ouders/boeken/toevoegen">
+                    {{ csrf_field() }}
                     <div class="card">
                         <h4>Titel</h4>
                         <input required name="title" type="text" class="input" id="title" placeholder="Boek titel*" class="input">
                     </div>
                     <div class="card">
+                        <h4>Auteur</h4>
+                        <input type="text" placeholder="Auteur*" autocorrect="off" autocapitalize="off" spellcheck="off" class="input">
+                    </div>                    
+                    <div class="card">
                         <h4>Korte samenvatting</h4>
                         <textarea required name="description" type="text" class="input" id="description" placeholder="Korte beschrijving*" class="input"></textarea>
                     </div>
+                    <div class="card">
+                        <h4>Aantal bladzijden</h4>
+                        <input required name="numberOfPages" type="number" class="input" id="numberOfPages" placeholder="Aantal bladzijden*">
+                    </div>
                 </form>
                 <div class="actions">
-                    <button  form="addBookForm" class="button">Add</button>
-                    <button  class="button cancel closeModal" onclick="closeModal(this);">Cancel</button>
+                    <button  form="addBookForm" class="button">Toevoegen</button>
+                    <button  class="button cancel closeModal" onclick="closeModal(this);">Annuleren</button>
                 </div>
             </div>
         </div>
